46a48643793d34532c515a01cdf646348672bca6
dwarkesh-test-ac <otp_by_travel1@proton.me>
2025-12-16 22:32:12 +0530

    Update update.js

--- api/update.js
+++ api/update.js
@@ -4,19 +4,51 @@ export default async function handler(req, res) {
   try {
     await connectDatabase();
 
-    // Check time
-    const hour = new Date().getHours();
-    const statusText = hour >= 12 ? "Evening" : "Morning";
+ let key=new Date().getDay()
+    let day="";
+    switch (key) {
+        case 1:
+            day="Monday"
+            break;
+    
+        case 2:
+            day="Tuesday"
+            break;
+    
+        case 3:
+            day="Wednesday"
+            break;
+    
+        case 4:
+            day="Thursday"
+            break;
+    
+        case 5:
+            day="Friday"
+            break;
+    
+        case 6:
+            day="Saturday"
+            break;
+    
+        case 7:
+            day="Sunday"
+            break;
+    
+        default:
+            day="Error: Please pay attention to the problem."
+            break;
+    }
 
     console.log("CRON TRIGGERED AT:", new Date().toISOString());
 
     const result = await client.query(
       "UPDATE active SET active=$1 WHERE id=$2",
-      [statusText, 1]
+      [day, 1]
     );
 
     return res.status(200).json({
-      message: `Updated to ${statusText}`,
+      message: `Updated to ${day}`,
       result,
     });
   } catch (error) {
@@ -24,5 +56,6 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: error.message });
   }
 }
+
 
 
